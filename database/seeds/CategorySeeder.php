96bf0916f3fa7bbce6c2635026e99c0866685eff
VincenzoVitello <vincenzovitellolavoro@gmail.com>
2022-04-12 15:27:34 +0200

    category seeder creato e inseminato

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['pizza', 'carne', 'pesce', 'sushi', 'poke', 'hamburger', 'kebab'];
+        foreach($categories as $element){
+            $new_category = new Category();
+            $new_category->name = $element;
+            // creiamo seed dello slug (?) : $new_category->slug = Str::of($element)->slug('-');
+            $new_category->save();
+        }
+    }
+}
