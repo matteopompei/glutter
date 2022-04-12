768c0aed1ab5e71fead3472fa9743ab04770eecb
Matteo Pompei <datxizzan@gmail.com>
2022-04-12 18:43:09 +0200

    Layout dashboard abbozzato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+  <div class="container">
+    <div class="dashboard">
+      <div class="row">
+        <div class="col">
+          <div class="title">
+            <h3><i class="fa-solid fa-angle-right"></i> Dashboard</h3>
+          </div>
         </div>
+      </div>
+      <div class="row">
+        <div class="col-4 info">
+          <div class="text-center title">
+            <h4>Riepilogo informazioni</h4>
+          </div>
+          <img
+            src="https://loredanavistarini.it/wp-content/uploads/2020/01/136-1366211_group-of-10-guys-login-user-icon-png.png"
+            alt="Avatar" class="img-fluid avatar">
+          <ul>
+            <li><strong>Nome attività: </strong> placeholder</li>
+            <li><strong>Indirizzo: </strong> placeholder</li>
+            <li><strong>Email: </strong> placeholder</li>
+            <li><strong>Partita IVA: </strong> placeholder</li>
+            <li><strong>Categorie: </strong> placeholder</li>
+          </ul>
+          <a href="#" class="btn btn-primary edit">Modifica informazioni</a>
+        </div>
+        <div class="col-8 welcome">
+          <h2>Bentornato!</h2>
+          <div class="row">
+            <div class="col"><a href="#" class="btn btn-primary">Visualizza menù</a></div>
+            <div class="col"><a href="#" class="btn btn-primary">Visualizza ordini</a></div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
