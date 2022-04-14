12a329f1616b27a58bbe888d95d2fd01241a76e9
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-04-14 20:38:03 +0200

    Aggiunto boostrap al font (stavolta davvero)

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -25,6 +25,8 @@ export default {
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css";
 // @import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css";
 
+@import "~bootstrap/scss/bootstrap";
+
 * {
     box-sizing: border-box;
     padding: 0;
