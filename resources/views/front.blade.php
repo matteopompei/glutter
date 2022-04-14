0b7e8bf5eb3314ba4fb1bf7fbad83095ecc4e954
Matteo Pompei <datxizzan@gmail.com>
2022-04-14 21:25:14 +0200

    Tentativo di collegare il JS

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -6,12 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Glutter</title>
+  <link rel="stylesheet" href="{{ asset('css/front.css') }}">
 </head>
 
 <body>
   <div id="app"></div>
 
-  <script src="{{ asset('js/front.js') }}"></script>
+  <script src="{{ asset('js/front.js') }}" defer></script>
 </body>
 
 </html>
