e7ad60fa47b434a37484eb178b3e4023d0dc0580
Matteo Pompei <datxizzan@gmail.com>
2022-04-15 12:01:33 +0200

    Grafica fatta

--- resources/views/auth/dish/edit.blade.php
+++ resources/views/auth/dish/edit.blade.php
@@ -1,38 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<form action="{{route('auth.dish.update', $dish->id)}}" method="POST" enctype="multipart/form-data">
+  <div class="container">
+    <div class="dashboard">
+      <div class="row">
+        <div class="col">
+          <div class="title mb-5">
+            <h3><i class="fa-solid fa-angle-right"></i> Modifica piatto</h3>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col left">
+          <form action="{{ route('auth.dish.update', $dish->id) }}" method="POST" enctype="multipart/form-data">
 
-    @csrf
-    @method('PUT')
+            @csrf
+            @method('PUT')
 
-    <div class="form-group">
-        <label for="title">Nome piatto</label>
-        <input class="form-control" type="text" placeholder="Nome del piatto" id="name" name="name" value="{{$dish->name}}">
+            <div class="form-group">
+              <label for="title">Nome piatto</label>
+              <input class="form-control" type="text" placeholder="Nome del piatto" id="name" name="name"
+                value="{{ $dish->name }}">
+            </div>
+
+            <div class="form-group">
+              <label for="content">Ingredienti</label>
+              <textarea class="form-control" id="ingredients" name="ingredients" rows="30"
+                placeholder="Descrizione del piatto">{{ $dish->ingredients }}</textarea>
+            </div>
+
+            <div class="form-group">
+              <label for="ingredients">Prezzo</label>
+              <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price"
+                value="{{ $dish->price }}" rows="1" placeholder="Prezzo">
+            </div>
+
+            <div class="form-group">
+              <label for="image">Image</label>
+              <input type="file" class="form-control-file" name="image" id="image">
+            </div>
+
+            <div class="text-center mt-5 mb-1">
+              <button type="submit" class="btn btn btn btn-primary mr-2">Salva le modifiche</button>
+              <a href="{{ route('auth.dish.index') }}" class="btn btn-danger">Indietro</a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-
-    <div class="form-group">
-        <label for="content">Ingredienti</label>
-        <textarea class="form-control" id="ingredients" name="ingredients" rows="30" placeholder="Descrizione del piatto">{{$dish->ingredients}}</textarea>
-    </div>
-
-    <div class="form-group">
-        <label for="ingredients">Prezzo</label>
-        <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{$dish->price}}" 
-        rows="1" placeholder="Prezzo">
-    </div>
-
-    <div class="form-group">
-        <label for="image">Image</label>
-        <input type="file" class="form-control-file" name="image" id="image">
-    </div>
-
-    <div class="text-center">
-        <button type="submit" class="btn btn-secondary my-5 text-center">Salva</button>
-    </div>
-
-    <div class="text-center">
-        <a href="{{route("auth.dish.index")}}"><button type="button" class="btn btn-secondary">Torna alla home</button></a>
-    </div>
-</form>
+  </div>
 @endsection
