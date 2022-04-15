ab09fab53480ba9377cb9e6d8c36bd2ba8cc32f3
Matteo Pompei <datxizzan@gmail.com>
2022-04-15 11:37:00 +0200

    Codice migliorato e tabella responsive

--- resources/views/auth/dish/index.blade.php
+++ resources/views/auth/dish/index.blade.php
@@ -12,46 +12,45 @@
       </div>
       <div class="row">
         <div class="col left">
-          <table class="table table-white table-hover">
-            <thead class="thead-light">
-              <tr>
-                <th class="align-middle text-center" scope="col">ID</th>
-                <th class="align-middle text-center" scope="col">Nome piatto</th>
-                <th class="align-middle text-center" scope="col">Modifica il piatto</th>
-                <th class="align-middle text-center" scope="col">Visualizza il piatto</th>
-                <th class="align-middle text-center" scope="col">Elimina il piatto</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              @foreach ($dishes as $dish)
+          <div class="table-responsive">
+            <table class="table table-white table-hover">
+              <thead class="thead-light">
                 <tr>
-                  <th class="align-middle text-center" scope="row">{{ $dish['id'] }}</th>
-                  <td class="align-middle text-center">{{ $dish['name'] }}</td>
-                  <td class="align-middle text-center"><a href="{{ route('auth.dish.edit', $dish->id) }}"><button
-                        type="button" class="btn btn-success">Modifica</button></a></td>
-                  <td class="align-middle text-center"><a href="{{ route('auth.dish.show', $dish->id) }}"><button
-                        type="button" class="btn btn-info text-white">Visualizza</button></a></td>
-                  <td class="align-middle text-center">
-                    <form action="{{ route('auth.dish.destroy', $dish->id) }}" method="post">
-
-                      @csrf
-                      @method("DELETE")
-                      <button type="submit" class="btn btn-danger"
-                        onclick="return confirm('Sei sicuro? Il prodotto verrà eliminato definitivamente.')">Delete</button>
-
-                    </form>
-                  </td>
-
+                  <th class="align-middle text-center" scope="col">Nome piatto</th>
+                  <th class="align-middle text-center" scope="col">Modifica il piatto</th>
+                  <th class="align-middle text-center" scope="col">Visualizza il piatto</th>
+                  <th class="align-middle text-center" scope="col">Elimina il piatto</th>
                 </tr>
-              @endforeach
-            </tbody>
-          </table>
+              </thead>
+
+              <tbody>
+                @foreach ($dishes as $dish)
+                  <tr>
+                    <td class="align-middle text-center">{{ $dish['name'] }}</td>
+                    <td class="align-middle text-center"><a href="{{ route('auth.dish.edit', $dish->id) }}"
+                        class="btn btn-success">Modifica</a></td>
+                    <td class="align-middle text-center"><a href="{{ route('auth.dish.show', $dish->id) }}"
+                        class="btn btn-info text-white">Visualizza</a></td>
+                    <td class="align-middle text-center">
+                      <form action="{{ route('auth.dish.destroy', $dish->id) }}" method="post">
+
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="btn btn-danger"
+                          onclick="return confirm('Sei sicuro? Il prodotto verrà eliminato definitivamente.')">Delete</button>
+
+                      </form>
+                    </td>
+
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
 
           <div class="text-center mt-5 mb-1">
-            <a href="{{ route('auth.dish.create') }}"><button type="button" class="btn btn-primary mr-2">Aggiungi piatto
-                al
-                menù</button></a>
+            <a href="{{ route('auth.dish.create') }}" class="btn btn-primary mr-2">Aggiungi piatto
+              al menù</a>
             <a href="/dashboard" class="btn btn-danger">Indietro</a>
           </div>
         </div>
