2e6880eba31079220f2860e84a21a56f1177ad03
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-04-19 18:33:27 +0200

    Correzione codice

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Category;
 use App\Http\Controllers\Controller;
 use App\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
