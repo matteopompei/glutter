65ec8c8d2bfd299bdca2bf752be0ace7c88570b2
Lorenzo Bernardini <lorenzo.bernardini88@gmail.com>
2022-04-20 17:29:56 +0200

    prova users seeder

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -15,10 +15,10 @@ class UsersSeeder extends Seeder
     {
         $users=[
             [
-            'id'=> 0,
+            //'id'=> 0,
             'business_name'=> "Da Lorenzo",
             'address'=>'Corso Tacito, 38, Terni, TR, 05100',
-            'iamge'=>NULL, 
+            'image'=>NULL, 
             'categories'=>[8],
             'password'=>bcrypt('mirtillomirtillo'),
             'email'=> 'da@lorenzo.it',
@@ -74,12 +74,12 @@ class UsersSeeder extends Seeder
                 'price'=> 12,
                 'visible'=>true],
             ]
-            ],
+        ],
             [
-                'id'=> 1,
+                //'id'=> 1,
                 'business_name'=> "Da Matteo",
                 'address'=>'Via Battisti, 2, Castorano, AP, 63081',
-                'iamge'=>NULL, 
+                'image'=>NULL, 
                 'categories'=>[1],
                 'password'=>bcrypt('samsamsam'),
                 'email'=> 'da@matteo.it',
@@ -135,12 +135,12 @@ class UsersSeeder extends Seeder
                     'price'=> 10,
                     'visible'=>true],
                 ]
-                ],
-            [
-                'id'=> 2,
+            ],
+                [
+                //'id'=> 2,
                 'business_name'=> "Sushi Love",
                 'address'=>'Via della Stazione, 10, Roma, RO, 00100',
-                'iamge'=>NULL, 
+                'image'=>NULL, 
                 'categories'=>[3],
                 'password'=>bcrypt('mirtillosam'),
                 'email'=> 'sushi@love.it',
@@ -179,14 +179,15 @@ class UsersSeeder extends Seeder
         ];
         foreach($users as $user){
             $new_user = new User();
-            $new_user->id = $user['id'];
             $new_user->business_name = $user['business_name'];
             $new_user->address = $user['address'];
             $new_user->email = $user['email'];
             $new_user->password = $user['password'];
             $new_user->p_iva = $user['p_iva'];
-            $new_user->image = $user['iamge'];
-            $new_user->categories = $user[''];
+            $new_user->image = $user['image'];
+            $new_user->save();
+            
+            $new_user->categories()->attach($category_id);
 
         }
     }
