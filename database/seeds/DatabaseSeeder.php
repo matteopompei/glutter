8a1f4f13278bafe1ca96df9d73a50caafc233833
Lorenzo Bernardini <lorenzo.bernardini88@gmail.com>
2022-04-20 18:25:56 +0200

    dishes seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CategorySeeder::class);
         $this->call(UsersSeeder::class);
+        $this->call(DishesSeeder::class);
     }
 }
