919687aff28ec0bbebe17486bead79138c292ff3
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-04-21 05:01:50 +0200

    Iniziato stile carrello

--- resources/js/components/Cart.vue
+++ resources/js/components/Cart.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="navbar-item has-dropdown is-hoverable">
-    <div>Carrello ({{ $store.state.cartCount }})</div>
+    <div class="cart">
+      <i class="fa-solid fa-cart-shopping mx-4" />
+      <div class="cart-counter">{{ $store.state.cartCount }}</div>
+    </div>
 
     <div v-if="$store.state.cartCount > 0">
+      <div>Stai acquistando presso: Nome ristorante</div>
       <div
         v-for="dish in $store.state.cart"
         :key="'cart' + dish.id + dish.name"
@@ -37,5 +41,30 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.cart {
+  color: #fff;
+  transition: 0.3s;
+  cursor: pointer;
+  position: relative;
+
+  &:hover,
+  &:active {
+    transform: scale(1.1);
+  }
+
+  i {
+    font-size: 2rem;
+  }
+  .cart-counter {
+    font-size: 15px;
+    padding: 0 7px;
+    position: absolute;
+    right: 10px;
+    bottom: -10px;
+
+    background-color: red;
+    border-radius: 50%;
+  }
+}
 </style>
