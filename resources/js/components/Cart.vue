d8329c48c16827e2bb1796db55c5f9edf8cb5937
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-04-21 04:37:39 +0200

    Aggiunto carrello nuovo

---
+++ resources/js/components/Cart.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="navbar-item has-dropdown is-hoverable">
+    <div>Carrello ({{ $store.state.cartCount }})</div>
+
+    <div v-if="$store.state.cartCount > 0">
+      <div
+        v-for="dish in $store.state.cart"
+        :key="'cart' + dish.id + dish.name"
+        class="navbar-item"
+        href=""
+      >
+        {{ dish.name }} x{{ dish.quantity }} ({{ dish.totalPrice }}€)
+      </div>
+
+      <div class="navbar-item" href="">Totale: {{ totalPrice }}€</div>
+
+      <button class="button is-success" @click.prevent="removeAllFromCart()">
+        Svuota Carrello
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cart",
+  methods: {
+    removeAllFromCart() {
+      this.$store.commit("removeAllFromCart");
+    },
+  },
+  computed: {
+    totalPrice() {
+      return this.$store.getters.getTotal;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,51 +1,49 @@
 <template>
-    <header>
-        <nav class="navbar navbar-expand-md navbar-dark py-3 fixed-top">
-            <a class="navbar-brand" href="/">
-                <img
-                    src="/images/Glutter1.png"
-                    alt="Glutter"
-                    class="img-fluid logo"
-                />
-            </a>
+  <header>
+    <nav class="navbar navbar-expand-md navbar-dark py-3 fixed-top">
+      <a class="navbar-brand" href="/">
+        <img src="/images/Glutter1.png" alt="Glutter" class="img-fluid logo" />
+      </a>
 
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span class="navbar-toggler-icon"></span>
-            </button>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-            <div
-                class="collapse navbar-collapse justify-content-end"
-                id="navbarSupportedContent"
-            >
-                <div
-                    class="form-inline mt-5 mb-3 my-md-2 justify-content-center user"
-                >
-                    <a href="/login" class="btn mx-2">Accedi</a>
-                    <a
-                        href="/register"
-                        class="btn btn-light text-uppercase mx-2"
-                        >Registrati</a
-                    >
-                    <ShoppingCart />
-                </div>
-            </div>
-        </nav>
-    </header>
+      <div
+        class="collapse navbar-collapse justify-content-end"
+        id="navbarSupportedContent"
+      >
+        <div class="form-inline mt-5 mb-3 my-md-2 justify-content-center user">
+          <a href="/login" class="btn mx-2">Accedi</a>
+          <a href="/register" class="btn btn-light text-uppercase mx-2"
+            >Registrati</a
+          >
+          <!-- <ShoppingCart /> -->
+          <Cart />
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
-import ShoppingCart from "./ShoppingCart.vue";
+// import ShoppingCart from "./ShoppingCart.vue";
+import Cart from "./Cart.vue";
+
 export default {
-    name: "Header",
-    components: { ShoppingCart },
+  name: "Header",
+  components: {
+    //ShoppingCart,
+    Cart,
+  },
 };
 </script>
 
@@ -53,12 +51,12 @@ export default {
 @import "../../sass/_variables.scss";
 
 nav {
-    background: rgba($color: $color4, $alpha: 0.7);
-    backdrop-filter: blur(10px);
-    box-shadow: 0 5px 10px rgba($color: #000000, $alpha: 0.1);
+  background: rgba($color: $color4, $alpha: 0.7);
+  backdrop-filter: blur(10px);
+  box-shadow: 0 5px 10px rgba($color: #000000, $alpha: 0.1);
 
-    .logo {
-        max-width: 250px;
-    }
+  .logo {
+    max-width: 250px;
+  }
 }
 </style>
