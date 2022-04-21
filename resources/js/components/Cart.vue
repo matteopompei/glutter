eef8c024a857cde70cc508d9b3ea53fcc8c4c07d
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-04-21 12:35:55 +0200

    Aggiunto modal carrello

--- resources/js/components/Cart.vue
+++ resources/js/components/Cart.vue
@@ -1,26 +1,8 @@
 <template>
   <div class="navbar-item has-dropdown is-hoverable">
     <div class="cart">
-      <i class="fa-solid fa-cart-shopping mx-4" />
+      <i @click="showModal" class="fa-solid fa-cart-shopping mx-4" />
       <div class="cart-counter">{{ $store.state.cartCount }}</div>
-    </div>
-
-    <div v-if="$store.state.cartCount > 0">
-      <div>Stai acquistando presso: Nome ristorante</div>
-      <div
-        v-for="dish in $store.state.cart"
-        :key="'cart' + dish.id + dish.name"
-        class="navbar-item"
-        href=""
-      >
-        {{ dish.name }} x{{ dish.quantity }} ({{ dish.totalPrice }}€)
-      </div>
-
-      <div class="navbar-item" href="">Totale: {{ totalPrice }}€</div>
-
-      <button class="button is-success" @click.prevent="removeAllFromCart()">
-        Svuota Carrello
-      </button>
     </div>
   </div>
 </template>
@@ -29,13 +11,8 @@
 export default {
   name: "Cart",
   methods: {
-    removeAllFromCart() {
-      this.$store.commit("removeAllFromCart");
-    },
-  },
-  computed: {
-    totalPrice() {
-      return this.$store.getters.getTotal;
+    showModal() {
+      this.$root.$emit("Main");
     },
   },
 };
