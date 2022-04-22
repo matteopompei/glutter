9c6c2089ddd1404bb97e6dcae5986ba48cb930f7
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-04-22 11:22:58 +0200

    Aggiunta variabile userID

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -67,12 +67,12 @@
           >
             Svuota Carrello
           </button>
-          <a
-            :href="`/restaurant/${this.$store.state.cart[0].user_id}`"
+          <button
             class="btn ms_btn_dismiss"
           >
-            Torna al ristorante
-          </a>
+            <!-- <router-link :to="`/restaurant/${this.$store.state.cart[0].user_id}`">Torna al ristorante</router-link> -->
+            <router-link :to="{ name: 'singleuser', params: { id:$store.state.userID } }">Torna al ristorante</router-link>
+          </button>
           <button type="button" class="btn ms_btn_checkout">
             Vai al checkout
           </button>
