a953680c6e4d485526aee8464dadecbeaef53d46
Matteo Pompei <datxizzan@gmail.com>
2022-04-22 12:19:39 +0200

    Sfumatura aggiunta

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,30 +1,30 @@
 <template>
-  <main>
-    <div class="nav-space"></div>
+    <main>
+        <div class="nav-space"></div>
 
-    <router-view></router-view>
-    <Modal ref="modal" />
-  </main>
+        <router-view></router-view>
+        <Modal ref="modal" />
+    </main>
 </template>
 
 <script>
 import Modal from "./Modal.vue";
 export default {
-  name: "Main",
-  components: {
-    Modal,
-  },
-  methods: {
-    showModal() {
-      let element = this.$refs.modal.$el;
-      $(element).modal("show");
+    name: "Main",
+    components: {
+        Modal,
     },
-  },
-  mounted() {
-    this.$root.$on("Main", () => {
-      this.showModal();
-    });
-  },
+    methods: {
+        showModal() {
+            let element = this.$refs.modal.$el;
+            $(element).modal("show");
+        },
+    },
+    mounted() {
+        this.$root.$on("Main", () => {
+            this.showModal();
+        });
+    },
 };
 </script>
 
@@ -32,9 +32,10 @@ export default {
 @import "../../sass/_variables.scss";
 
 main {
-  .nav-space {
-    padding-top: 100px;
-    background: $color4;
-  }
+    .nav-space {
+        padding-top: 100px;
+        background: $color4;
+        box-shadow: 0 10px 40px 30px $color4;
+    }
 }
 </style>
