709b253d21d377006530a977e18374a8d14a3150
Your Name <yan.vi193@gmail.com>
2022-04-25 17:28:50 +0200

    fix-show-ordine

--- resources/views/auth/order/show.blade.php
+++ resources/views/auth/order/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-    {{ $order->id }}
+    {{ $order->name }}
 @endsection
 
 @section('content')
@@ -16,9 +16,60 @@
             </div>
             <div class="row">
                 <div class="col left">
+                    <div>
+                        <table class="table table-white table-hover">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th class="align-middle text-center" scope="col">ID</th>
+                                    <th class="align-middle text-center" scope="col">e-mail cliente</th>
+                                    <th class="align-middle text-center" scope="col">Nome cliente</th>
+                                    <th class="align-middle text-center" scope="col">Numero di telefono</th>
+                                    <th class="align-middle text-center" scope="col">Indirizzo</th>
+                                    <th class="align-middle text-center" scope="col">Spese di spedizione</th>
+                                    <th class="align-middle text-center" scope="col">Totale ordine</th>
+                                    <th class="align-middle text-center" scope="col">Pagato</th>
+                                    <th class="align-middle text-center" scope="col">Elimina ordine</th>
+                                </tr>
+                            </thead>
+                            
+                            <tbody>
+                                    <tr>
+                                        <td class="align-middle text-center">{{ $order['id'] }}</td>
+                                        <td class="align-middle text-center">{{ $order['email'] }}</td>
+                                        <td class="align-middle text-center">{{ $order['name'] }}</td>
+                                        <td class="align-middle text-center">{{ $order['phone'] }}</td>
+                                        <td class="align-middle text-center">{{ $order['address'] }}</td>
+                                        <td class="align-middle text-center">{{ $order['shipment'] }}</td>
+                                        <td class="align-middle text-center">{{ $order['total'] }}€</td>
+                                        <td class="align-middle text-center">
+                                          @if ($order->payed == 0)
+                                            <div>
+                                                <span>No</span>
+                                            </div>
+                                          @else
+                                            <div>
+                                                <span>Si</span>
+                                            </div>
+                                          @endif
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <form action="{{ route('auth.order.destroy', $order->id) }}" method="post">
 
+                                                @csrf
+                                                @method("DELETE")
+                                                <button type="submit" class="btn btn-danger"
+                                                    onclick="return confirm('Sei sicuro? Questo ordine verrà eliminato definitivamente.')">Delete</button>
+
+                                            </form>
+                                        </td>
+
+                                    </tr>
+                            </tbody>
+
+                        </table>
+                    </div>
                     <div class="text-center mt-5 mb-1">
-                        <a href="/dashboard"  class="btn btn-danger">Indietro</a>
+                        <a href="/dashboard" class="btn btn-danger">Indietro</a>
                     </div>
                 </div>
             </div>
