8e9ed3820f37e95e5db61f0e5b823c56999eb69d
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-04-25 12:57:49 +0200

    Riordinate colonne

--- database/migrations/2022_04_11_092440_create_orders_table.php
+++ database/migrations/2022_04_11_092440_create_orders_table.php
@@ -16,8 +16,8 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('email');
             $table->string('name');
+            $table->string('email');
             $table->string('phone');
             $table->string('address');
             $table->string('shipment');
