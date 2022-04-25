b0cbbad995c5b80ba0e80924a3a97df8bf78b227
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-04-25 05:31:57 +0200

    Creato Controller ordini

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Order;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        $orders = Auth::user()->orders;
+
+        return view('auth.orders.index', compact('orders'));
+    }
+
+    public function show(Order $order)
+    {
+        return view('auth.orders.show', compact('order'));
+    }
 }
