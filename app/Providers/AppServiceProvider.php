5c37d02f26d579044dee75e86b3196df4884199e
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-04-25 03:58:15 +0200

    Aggiunta configurazione braintree

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Braintree;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Braintree\Configuration::environment(env('BRAINTREE_ENV'));
+        \Braintree\Configuration::merchantId(env('BRAINTREE_MERCHANT_ID'));
+        \Braintree\Configuration::publicKey(env('BRAINTREE_PUBLIC_KEY'));
+        \Braintree\Configuration::privateKey(env('BRAINTREE_PRIVATE_KEY'));
     }
 }
