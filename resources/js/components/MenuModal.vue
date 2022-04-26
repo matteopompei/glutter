e6fc4f4c5631e4f2d00a4d4baeb2f49d0dfea502
Matteo Pompei <datxizzan@gmail.com>
2022-04-26 18:26:15 +0200

    Grafica modal piatti fatta

--- resources/js/components/MenuModal.vue
+++ resources/js/components/MenuModal.vue
@@ -1,131 +1,164 @@
 <template>
-  <div
-    class="modal fade"
-    id="MenuModal"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">{{ dish.name }}</h5>
-          <button
-            type="button"
-            class="close"
-            data-dismiss="modal"
-            aria-label="Close"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <img
-            v-if="dish.image"
-            :src="`/storage/${dish.image}`"
-            :alt="dish.name"
-            class="card-img-top img-food"
-          />
-          <img
-            v-else
-            src="/images/dish-placeholder.png"
-            :alt="dish.name"
-            class="card-img-top img-food"
-          />
-          <p class="card-text">
-            {{ dish.ingredients }}
-          </p>
+    <div
+        class="modal fade"
+        id="piatto"
+        tabindex="-1"
+        aria-labelledby="piatto"
+        aria-hidden="true"
+    >
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content box-piatto">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">
+                        {{ dish.name }}
+                    </h5>
+                    <button
+                        type="button"
+                        class="close"
+                        data-dismiss="modal"
+                        aria-label="Close"
+                    >
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="avatar-container">
+                        <img
+                            v-if="dish.image"
+                            :src="`/storage/${dish.image}`"
+                            :alt="dish.name"
+                            class="rounded img-fluid img-food"
+                        />
+                        <img
+                            v-else
+                            src="/images/dish-placeholder.png"
+                            :alt="dish.name"
+                            class="rounded img-fluid img-food"
+                        />
+                    </div>
+                    <p class="my-4">
+                        {{ dish.ingredients }}
+                    </p>
 
-          <h5 v-if="dish['price']" class="text-right mt-4">
-            {{ formatPrice(dish.price) }} €
-          </h5>
+                    <h5 v-if="dish['price']" class="text-right">
+                        {{ formatPrice(dish.price) }} €
+                    </h5>
+                </div>
+                <div class="modal-footer d-flex justify-content-center">
+                    <button
+                        class="btn rounded-circle mx-2 py-1 px-2 add_btn"
+                        @click.prevent="addToCart(dish)"
+                    >
+                        <i class="fa-solid fa-plus"></i>
+                    </button>
+                    <div v-if="quantity" class="quantity">{{ quantity }}</div>
+                    <div v-else>0</div>
+                    <button
+                        class="btn rounded-circle mx-2 py-1 px-2 remove_btn"
+                        @click.prevent="removeFromCart(dish)"
+                    >
+                        <i class="fa-solid fa-minus"></i>
+                    </button>
+                </div>
+            </div>
         </div>
-        <div class="modal-footer d-flex justify-content-center">
-          <button class="button btn add_btn" @click.prevent="addToCart(dish)">
-            +
-          </button>
-          <div v-if="quantity" class="quantity">{{ quantity }}</div>
-          <div v-else>0</div>
-          <button
-            class="removeBtn btn remove_btn"
-            @click.prevent="removeFromCart(dish)"
-          >
-            -
-          </button>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "MenuModal",
-  data() {
-    return {
-      dish: {},
-      user: {},
-    };
-  },
-  methods: {
-    showMenuModal() {
-      this.$root.$emit("DishModalEvent");
+    name: "MenuModal",
+    data() {
+        return {
+            dish: {},
+            user: {},
+        };
     },
-    addToCart(dish) {
-      let businessName = this.user.business_name;
-      this.$store.commit("addToCart", { dish, businessName });
+    methods: {
+        showMenuModal() {
+            this.$root.$emit("DishModalEvent");
+        },
+        addToCart(dish) {
+            let businessName = this.user.business_name;
+            this.$store.commit("addToCart", { dish, businessName });
+        },
+        removeFromCart(dish) {
+            this.$store.commit("removeFromCart", dish);
+        },
+        formatPrice(price) {
+            console.log(price);
+            return price.replace(".", ",");
+        },
     },
-    removeFromCart(dish) {
-      this.$store.commit("removeFromCart", dish);
+    computed: {
+        totalPrice() {
+            return this.$store.getters.getTotal;
+        },
+        quantity() {
+            for (let item of this.$store.state.cart) {
+                if (item.id == this.dish.id) {
+                    return item.quantity;
+                } else {
+                    return 0;
+                }
+            }
+        },
     },
-    formatPrice(price) {
-      console.log(price);
-      return price.replace(".", ",");
+    mounted() {
+        this.$root.$on("DishModalEvent", (data) => {
+            this.dish = data.dish;
+            this.user = data.user;
+        });
     },
-  },
-  computed: {
-    totalPrice() {
-      return this.$store.getters.getTotal;
-    },
-    quantity() {
-      for (let item of this.$store.state.cart) {
-        if (item.id == this.dish.id) {
-          return item.quantity;
-        } else {
-          return 0;
-        }
-      }
-    },
-  },
-  mounted() {
-    this.$root.$on("DishModalEvent", (data) => {
-      this.dish = data.dish;
-      this.user = data.user;
-    });
-  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../../sass/_variables.scss";
-.add_btn {
-  background-color: $color3;
-  color: #fff;
-}
-.remove_btn {
-  background-color: $red;
-  color: #fff;
-}
-.ms_btn_dismiss {
-  color: white;
-  background-color: $grey3;
-}
-.ms_btn_checkout {
-  color: white;
-  background-color: $color3;
-}
-.btn_scoop {
-  background-color: $red;
-  color: white;
+
+#piatto {
+    background: rgba($color: $black, $alpha: 0.2);
+
+    .box-piatto {
+        box-shadow: 0 10px 40px rgba($color: $black, $alpha: 0.7);
+
+        .avatar-container {
+            padding-top: 50%;
+            position: relative;
+
+            .img-food {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                background-color: $grey1;
+            }
+        }
+
+        .add_btn,
+        .remove_btn {
+            background: $white;
+            box-shadow: 0 3px 5px rgba($color: $black, $alpha: 0.1);
+            transition: 0.3s;
+
+            &:hover {
+                box-shadow: 0 3px 10px rgba($color: $black, $alpha: 0.15);
+            }
+
+            &:active {
+                background: $grey1;
+                box-shadow: 0 0 3px rgba($color: $black, $alpha: 0.2);
+            }
+        }
+        .add_btn {
+            color: $color3;
+        }
+        .remove_btn {
+            color: $color2;
+        }
+    }
 }
 </style>
