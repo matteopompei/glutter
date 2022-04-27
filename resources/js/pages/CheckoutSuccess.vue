05138e6df3d8e34f992b4dd19780dcd90b67cde7
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-04-27 03:49:05 +0200

    Creata pagina successo checkout

---
+++ resources/js/pages/CheckoutSuccess.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Pagamento avvenuto con successo!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckoutSuccess",
+};
+</script>
+
+<style>
+</style>
